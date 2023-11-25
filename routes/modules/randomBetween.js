9f09065f734d8df2cd878980f0299fe222c276ca
HGanyu <phanthaihoa070707@gmail.com>
2023-11-25 19:58:23 +0700

    Moved backend code to another repo

--- routes/modules/randomBetween.js
+++
@@ -1,6 +0,0 @@
-function randomBetween(array) {
-    const randomizedIndex = Math.floor(Math.random() * (array.length))
-    return array[randomizedIndex]
-}
-
-module.exports = randomBetween
--- routes/v1/routesRoot.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-let router = express.Router()
-
-/* Routes */
-router.get('/', (req, res) => {
-    res.send("This is our API!")
-})
-
-module.exports = router
